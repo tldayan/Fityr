77676b85eb40890007629812fe3d18ed60e1f0b1
tldayan <110288123+tldayan@users.noreply.github.com>
2026-01-09 16:48:02 +0400

    Fixes

--- src/app/resetpassword/page.tsx
+++ src/app/resetpassword/page.tsx
@@ -1,74 +1,10 @@
-"use client";
+import { Suspense } from "react";
+import ResetPasswordClient from "./ResetPasswordClient";
 
-import CustomTextInput from "@/components/CustomTextInput/CustomTextInput";
-import { useStytch } from "@stytch/nextjs";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useState } from "react";
-import styles from "./page.module.css";
-import CustomButton from "@/components/CustomButton/CustomButton";
-import ButtonStyles from "@/app/globalStyles/buttonStyles.module.css";
-import toast from "react-hot-toast";
-
-export default function ResetPasswordClient() {
-  const searchParams = useSearchParams();
-  const token = searchParams.get("token");
-  const tokenType = searchParams.get("stytch_token_type");
-
-  const stytchClient = useStytch();
-  const router = useRouter();
-
-  const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
-
-  if (tokenType !== "reset_password" || !token) {
-    return <p>Invalid or expired reset link</p>;
-  }
-
-  const handleResetPassword = async () => {
-    setLoading(true);
-    setError("");
-
-    try {
-      const res = await stytchClient.passwords.resetByEmail({
-        token,
-        password,
-        session_duration_minutes: 60,
-      });
-
-      await stytchClient.session.revoke();
-
-      if (res.status_code === 200) {
-        toast.success("Password reset complete!");
-        router.push("/");
-      }
-    } catch (err: any) {
-      setError(err?.error_message || "Failed to reset password");
-    } finally {
-      setLoading(false);
-    }
-  };
-
+export default function Page() {
   return (
-    <div className={styles.container}>
-      <h3>Reset password</h3>
-
-      <CustomTextInput
-        disabled={loading}
-        type="password"
-        placeholder="New password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-
-      <CustomButton
-        title={loading ? "Resetting..." : "Reset Password"}
-        className={ButtonStyles.primary_button}
-        onClick={handleResetPassword}
-        disabled={loading}
-      />
-
-      {error && <p style={{ color: "red" }}>{error}</p>}
-    </div>
+    <Suspense fallback={<p>Loading reset password...</p>}>
+      <ResetPasswordClient />
+    </Suspense>
   );
 }
