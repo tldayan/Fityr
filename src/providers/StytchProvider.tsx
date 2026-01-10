ee200b438dafc3becdc8e9fbe22ee227f39b5e30
tldayan <110288123+tldayan@users.noreply.github.com>
2026-01-10 10:55:36 +0400

    Fixes

--- src/providers/StytchProvider.tsx
+++ src/providers/StytchProvider.tsx
@@ -4,15 +4,16 @@ import { StytchProvider } from '@stytch/nextjs';
 import { createStytchUIClient } from '@stytch/nextjs/ui';
 
 const stytch = createStytchUIClient(
-  process.env.NEXT_PUBLIC_STYTCH_PUBLIC_TOKEN!
+  process.env.NEXT_PUBLIC_STYTCH_PUBLIC_TOKEN!,
+  {
+    cookieOptions: {
+      domain: ".fityr.xyz",
+      path: "/",
+      availableToSubdomains: true,
+    },
+  }
 );
 
-
 export function StytchProviders({ children }: { children: React.ReactNode }) {
-
-  return (
-    <StytchProvider stytch={stytch}>
-      {children}
-    </StytchProvider>
-  );
+  return <StytchProvider stytch={stytch}>{children}</StytchProvider>;
 }
